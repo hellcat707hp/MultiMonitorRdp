293588da2d6bd55eacd4e13663dfdeaca1f0a154
Steven Curran <spcurran21@gmail.com>
2022-04-18 11:13:27 -0500

    Create repo.

---
+++ MultiMonitorRdp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+const string HelpText =
+    $"MultiMonitorRDP \n" +
+    $"    - First argument should be which monitors you want to use from left-to-right as a 0-based list \"0,1,2,3,...\".\n" +
+    $"          Example: For the two rightmost monitors in a 3 monitor setup, use \"1,2\".\n" +
+    $"          Note: RDP can only use select monitors that share an edge.\n" +
+    $"    - Second argument should be the path to the .rdp file you want to use for connection.";
+
+if (args == null || args.Length < 2)
+{
+    Console.WriteLine(HelpText);
+    return;
+}
+
+string[] desiredMonitorStrings = args[0].Split(',');
+if (desiredMonitorStrings.Length == 0) {
+    Console.WriteLine(HelpText);
+    return;
+}
+
+List<int> desiredMonitors = new List<int>(); 
+foreach(string argMon in desiredMonitorStrings)
+{
+    int outNum = -1;
+    bool success = int.TryParse(argMon, out outNum);
+    if (success)
+    {
+        desiredMonitors.Add(outNum);
+    }
+}
+
+if (desiredMonitors.Count == 0)
+{
+    //You didnt enter any valid monitor numbers
+    Console.WriteLine(HelpText);
+    return;
+}
+
+var displaysltr = (from display in Screen.AllScreens
+                   orderby display.Bounds.Left select display).ToList();
+
+
+//Figure out the actual display numbers that Windows is using so we can hand those to RDP
+List<int> displayNumbersToUse = new List<int>();
+foreach (var desiredDisplayNum in desiredMonitors)
+{
+    if (displaysltr.Count-1 < desiredDisplayNum)
+    {
+        Console.WriteLine($"The desired display \"{desiredDisplayNum}\" does not exist.");
+        return;
+    }
+    var display = displaysltr[desiredDisplayNum];
+    int displayNumber = int.Parse(Regex.Match(display.DeviceName, @"\d+").Value) - 1;
+    displayNumbersToUse.Add(displayNumber);
+}
+
+if (!File.Exists(args[1]))
+{
+    Console.WriteLine("The supplied RDP file does not exist.");
+    return;
+}
+
+//Write the monitor settings to the RDP file.
+string monitorString = string.Join(',', displayNumbersToUse);
+List<string> lines = File.ReadAllLines(args[1]).ToList();
+int monitorsLineToEdit = lines.FindIndex(x => x.StartsWith("selectedmonitors"));
+if (monitorsLineToEdit == -1)
+{
+    lines.Add($"selectedmonitors:s:{monitorString}");
+} 
+else
+{
+    lines[monitorsLineToEdit] = $"selectedmonitors:s:{monitorString}";
+}
+
+int useMultiLineToEdit = lines.FindIndex(x => x.StartsWith("use multimon"));
+if (useMultiLineToEdit == -1)
+{
+    lines.Add($"use multimon:i:1");
+}
+else
+{
+    lines[useMultiLineToEdit] = $"use multimon:i:1";
+}
+File.WriteAllLines(args[1], lines);
+
+//Start RDP with our updated connection file
+ProcessStartInfo startInfo = new ProcessStartInfo("mstsc.exe", $"\"{args[1]}\"");
+Process.Start(startInfo);
---
+++ README.md
@@ -0,0 +1,31 @@
+# MultiMonitorRdp
+MultiMonitorRdp is a simple console program designed to make using Windows Remote Desktop client easier with multi-display setups. This program only exists because Windows tends to change which display numbers are which between restarts, sleeps, and even displays waking up.
+___
+
+## Usage
+First, ensure you have a .rdp file available with all your existing settings. <br>
+> You can create one by opening Remote Desktop Connection, expanding "Show Options", and choosing to save your connection settings at the bottom of the "General" tab.
+
+Next, either download or build the program, then execute as follows:<br>
+`MultiMonitorRdp.exe *monitors_to_use* *path_to_rdp_file*`
+
+### Example
+
+You want to use just the two rightmost monitors of your 3-monitor setup. (See the diagram in [Notes](#notes))
+
+Execute the following command:<br>
+`MultiMonitorRdp.exe "1,2" *path_to_rdp_file*`
+
+## Notes
+- I can't guarantee this is going to work well with every monitor setup out there. This has been (and will be) only tested with horizontal monitors that go left-to-right and touch at the borders (see diagram below), which is a fairly standard setup.
+ ```
+ Triple Monitor Example
+  ___________  ___________  ___________ 
+ |           ||           ||           |
+ |     0     ||     1     ||     2     |
+ |___________||___________||___________| 
+       |            |            |      
+      
+ ```
+
+- Windows is very strange about display numbers even just waking up displays from sleep. Your remote session resizing randomly when waking up displays is totally "normal" and this program cannot fix that. You have to restart your remote session.
